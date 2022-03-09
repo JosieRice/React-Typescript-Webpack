7535ad879ba56a5f9a347d375081d4beb0cf06de
Chloë Rice <chloe@Chloes-MBP.hitronhub.home>
2022-03-08 19:12:11 -0500

    added a 404 page for routes that don't exist

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Signin from "./routes/signin/Index";
 import Home from "./routes/home/Index";
+import NotFound from "./NotFound";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="signin" element={<Signin />} />
+
+      {/* Not Found Route */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
