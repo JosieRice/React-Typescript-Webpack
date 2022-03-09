791b42638c89803634a8457735fd3332b6eee837
Chloë Rice <chloe@Chloes-MBP.hitronhub.home>
2022-03-08 19:08:31 -0500

    added a globl init for future use

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,12 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
+import init from "./init";
 
+/** everything that should be initialized globally on load goes here */
+init()
+
+/** global react entry point for wrappers that affect the whole app */
 ReactDOM.render(
   <StrictMode>
     <BrowserRouter>
---
+++ src/init.ts
@@ -0,0 +1,5 @@
+/** all things that should be initialized globally and before the app loads should be put in this function */
+const init = () => {
+};
+
+export default init;
